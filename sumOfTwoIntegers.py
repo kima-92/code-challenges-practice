2b1f1948b3cc79123e3d7c7f67b5aeead44c3697
Wilma <kima_92@fastmail.com>
2020-12-20 22:21:19 -0500

    Problem init

---
+++ sumOfTwoIntegers.py
@@ -0,0 +1,11 @@
+
+# Calculate the sum of two integers a and b, but you are NOT allowed to use the operator + and -.
+
+class Solution(object):
+    def getSum(self, a, b):
+        """
+        :type a: int
+        :type b: int
+        :rtype: int
+        """
+        
