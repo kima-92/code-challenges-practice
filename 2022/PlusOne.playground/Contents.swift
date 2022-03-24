de365e0a8064241b966b474181c845ec7981c225
Wilma <kima_92@fastmail.com>
2022-03-24 19:09:09 -0400

    Solve Plus One challenge

--- 2022/PlusOne.playground/Contents.swift
+++ 2022/PlusOne.playground/Contents.swift
@@ -9,19 +9,41 @@ import Foundation
 class Solution {
     func plusOne(_ digits: [Int]) -> [Int] {
         // if the array is empty, return an empty array
-        
         // Need the last element in the array
-            // if it's 9
-                // call this function again for the rest of the array,
-                // except for the last element
-                // save that new array in property
-                // append 0 at the end of the new array
-                // return new array
-            // else
-                // increment the last integer by 1
-                // return the array
+        guard let last = digits.last else { return digits }
         
-        // TODO: - Delete
-        return []
+        var mutableDigits = digits
+        
+        // if it's 9
+        if last == 9 {
+            // Prepare to call this function again for the rest of
+            // the array, except for the last element
+            mutableDigits.removeLast()
+            
+            // If 9 was the only element, return 10
+            if mutableDigits.isEmpty {
+                return [1, 0]
+            }
+            
+            // Save that new array in property
+            var reducedDigits = plusOne(mutableDigits)
+            // append 0 at the end of the new array
+            reducedDigits.append(0)
+            // return new array
+            return reducedDigits
+        } else {
+            // increment the last integer by 1
+            let updatedLast = last + 1
+            mutableDigits.removeLast()
+            mutableDigits.append(updatedLast)
+            // return the array
+            return mutableDigits
+        }
     }
 }
+
+let s = Solution()
+//let x = s.plusOne([4,3,2,1])
+//let x = s.plusOne([9, 9])
+let x = s.plusOne([4,3,2,1])
+print(x)
